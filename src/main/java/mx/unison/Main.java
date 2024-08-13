d906ff59520102051654e4a45a0c3b2ba8e65d93
Edgar1801 <a223204375@unison.mx>
2024-08-13 15:56:58 -0700

    se añade el proyecto

--- src/main/java/mx/unison/Main.java
+++ src/main/java/mx/unison/Main.java
@@ -4,9 +4,40 @@ package mx.unison;
 y cantos a asentamientos rurales
  */
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-        System.out.println("Hello world!");
+public class Main {
+
+    public static void main(String[] args) {
+        String archivoCSV = "codigos_postales.csv";
+        String linea = "";
+        String separador = ",";
+
+        int urbanos = 0;
+        int rurales = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivoCSV))) {
+            String cabecera = br.readLine();
+
+                while ((linea = br.readLine()) != null) {
+                String[] datos = linea.split(separador);
+
+                String tipoAsentamiento = datos[2];
+
+                if (tipoAsentamiento.equalsIgnoreCase("Urbano")) {
+                    urbanos++;
+                } else if (tipoAsentamiento.equalsIgnoreCase("Rural")) {
+                    rurales++;
+                }
+            }
+
+            System.out.println("Asentamientos urbanos: " + urbanos);
+            System.out.println("Asentamientos rurales: " + rurales);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
